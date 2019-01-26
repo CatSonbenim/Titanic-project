dc909b5a16a59c314d0aff8ee06ddb2ce648c6b5
Lisa Bulala <47056943+CatSonbenim@users.noreply.github.com>
2019-01-26 14:44:14 +0200

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import re
+
+
+def extract_name(full_name):
+    """ Функция очищает имена от "мусора". """
+
+    # первое слово в скобках
+    lam = lambda x: x.replace(full_name[x.find('\"'):x.rfind('\"') + 1:], '') if (x.find('\"') != -1) else x
+    name1 = re.search(".*\((.+)\).*", lam(full_name))
+    if name1 is not None:
+        return name1.group(1).split(" ")[0]
+    # первое слово после титула
+    name1 = re.search(".*\. ([A-Z,a-z]*)", lam(full_name))
+    return name1.group(1)
+
+
+data_frame = pd.read_csv('train.csv')[['Survived', 'Name', 'Sex']]
+alive = data_frame[data_frame.Survived == 1]
+dead = data_frame[data_frame.Survived == 0]
+alive_w = alive[alive.Sex == 'female']['Name']
+alive_m = alive[alive.Sex == 'male']['Name']
+dead_w = dead[dead.Sex == 'female']['Name']
+dead_m = dead[dead.Sex == 'male']['Name']
+
+# получаем имя с максимальной частотой
+r1 = alive_w.map(lambda full_name: extract_name(full_name)).value_counts().idxmax()
+r2 = alive_m.map(lambda full_name: extract_name(full_name)).value_counts().idxmax()
+r3 = dead_w.map(lambda full_name: extract_name(full_name)).value_counts().idxmax()
+r4 = dead_m.map(lambda full_name: extract_name(full_name)).value_counts().idxmax()
+
+print('\nСамые популярные имена среди выживших:\n'
+      'Женское:', r1,
+      '\nМужское:', r2,
+      '\n\nСамые популярные имена среди погибших:\n'
+      'Женское:', r3,
+      '\nМужское:', r4)
